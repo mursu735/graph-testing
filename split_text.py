c6d77da546d64099d68b56055776d73c150e932e
Sami Mairue <sami.mairue@aalto.fi>
2023-10-14 19:50:14 +0300

    Add script for splitting text to chapters

---
+++ split_text.py
@@ -0,0 +1,17 @@
+import os
+
+with open("input/around the world.txt", encoding="utf-8") as file:
+    lines = file.readlines()
+    file_name = 0
+    text = ""
+    for line in lines:
+        if "CHAPTER" in line:
+            if file_name > 0:
+                with open(f"input/Chapters/{file_name}.txt", "w", encoding="utf-8") as f:
+                    f.write(text)
+                    text = ""
+            file_name += 1
+        if not line.isupper():
+            text += line
+    with open(f"input/Chapters/{file_name}.txt", "w", encoding="utf-8") as f:
+        f.write(text)
