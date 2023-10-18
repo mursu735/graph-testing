f336399fbe5222dad31e561d37d92150c5ec60f0
Sami Mairue <sami.mairue@aalto.fi>
2023-10-19 01:17:28 +0300

    Add script for generating a prompt for testing

---
+++ prompt_builder.py
@@ -0,0 +1,26 @@
+import os
+
+
+chapter = int(input("Enter chapter (1-37):"))
+
+prompt = ""
+
+with open("input/instruction_location.txt", encoding="utf-8") as file:
+    prompt += file.read()
+
+prompt += "\n"
+if chapter > 1:
+    with open(f"output/GPT/summary/{chapter - 1}.txt", encoding="utf-8") as file:
+        text = file.read()
+        prompt += f"context:\n{text}"
+
+with open(f"input/Chapters/{chapter}.txt", encoding="utf-8") as file:
+    text = file.read()
+    prompt += f"prompt:\n{text}"
+
+#print(prompt)
+
+with open("prompt.txt", "w", encoding="utf-8") as file:
+    file.write(prompt)
+
+print("Prompt has been written to prompt.txt")
